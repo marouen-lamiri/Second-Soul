602b9b277c97fe8d20195b42bea41ea034460fac
Marouen Lamiri <marouen.lamiri@gmail.com>
2014-10-01 20:37:32 -0400

    Added Health Bar and Energy Bar
    This commit contains the Health Bar and Energy Bar but it is not
    completly functional yet as the bars do not decrease on the GUI. Also
    added a few buttons to heal, damage, decrease energy and increase energy
    for testing purposes

---
+++ Second Soul/Assets/Scripts/CharacterScripts/EnergyBar.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+#pragma strict
+var globeHeight:float;
+var globepic:Texture;
+var globesize:int;
+var energy:float;
+var maxEnergy:float;
+var energyPercent:float;
+
+function Start () {
+
+}
+
+function OnGUI () {
+	//Determining the health (through Percentage)
+	energyPercent = energy/maxEnergy;
+	if(energyPercent<0)
+	{
+		energyPercent = 0;
+	}
+	if(energyPercent>0)
+	{
+		energyPercent = 0;
+	}
+	globeHeight= energyPercent+globesize;
+	
+	//Drawing Energy Bar
+	GUI.BeginGroup(new Rect(Screen.width - 80, Screen.height-(globeHeight+20), globesize,globesize));
+	GUI.DrawTexture(Rect(0, -globesize+globeHeight, globesize, globesize),globepic);
+	GUI.EndGroup();
+	
+	if(GUI.Button(Rect(Screen.width/2 - 65,Screen.height-globesize, globesize, globesize), "Energy+"))
+	{
+		energy+=10;
+		if(energy>maxEnergy)
+			energy = maxEnergy;
+	}
+	if(GUI.Button(Rect(Screen.width/2 - 130,Screen.height-globesize, globesize, globesize), "Energy-"))
+	{
+		energy-=10;
+		if(energy<0)
+			energy = 0;
+	}	
+}
---
+++ Second Soul/Assets/Scripts/CharacterScripts/HealthBar.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+var globeHeight:float;
+var globepic:Texture;
+var globesize:int;
+var hp:float;
+var maxhp:float;
+var healthpercent:float;
+
+function Start () {
+	healthpercent = 1;
+}
+
+function OnGUI () {
+	//Determining the health (through Percentage)
+	healthpercent = hp/maxhp;
+	if(healthpercent<0)
+	{
+		healthpercent = 0;
+	}
+	if(healthpercent>0)
+	{
+		healthpercent = 0;
+	}
+	globeHeight= healthpercent+globesize;
+	
+	//Drawing health Bar
+	GUI.BeginGroup(new Rect(20, Screen.height-(globeHeight+20), globesize,globesize));
+	GUI.DrawTexture(Rect(0, -globesize+globeHeight, globesize, globesize),globepic);
+	GUI.EndGroup();
+	
+	if(GUI.Button(Rect(Screen.width/2,Screen.height-globesize, globesize, globesize), "Healing"))
+	{
+		hp+=10;
+		if(hp>maxhp)
+			hp = maxhp;
+	}
+	if(GUI.Button(Rect(Screen.width/2 + 65,Screen.height-globesize, globesize, globesize), "Damage"))
+	{
+		hp-=10;
+		if(hp<0)
+			hp = 0;
+	}	
+}
