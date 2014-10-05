e64f7ef792ce6e97f7eb09bfacfb9b8ae93d2772
gfortin13 <xguillaumex@hotmail.com>
2014-10-05 14:08:52 -0400

    Replaced base prototype for project
    Includes Features:
    -Click to move (with terrain object collision)
    -Fixed top-down camera follow
    -Single enemy spawn/chase
    -Player attack/kill enemy
    -enemy attack/kill player

---
+++ Second Soul/Assets/ClickToMove.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	public float speed;
+	public CharacterController controller;
+
+	private Vector3 position;
+
+	public AnimationClip run;
+	public AnimationClip idle;
+
+	public static bool attacking;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!attacking) {
+			if (Input.GetMouseButton (0)) {
+					//Locate player click position
+					locatePosition ();
+			}
+
+			//move player to position
+			moveToPosition ();
+		} 
+		else {
+
+		}
+	}
+
+	void locatePosition(){
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if(Physics.Raycast(ray, out hit, 1000)){
+			if(hit.collider.tag != "Player" && hit.collider.tag != "Enemy"){
+				position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			}
+			//Debug.Log(position);
+		}
+	}
+
+	void moveToPosition(){
+		//Player moving
+		if (Vector3.Distance (transform.position, position) > 1) {
+			Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
+
+			newRotation.x = 0;
+			newRotation.z = 0;
+
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 7);
+			controller.SimpleMove (transform.forward * speed);
+
+			animation.CrossFade(run.name);
+		}
+		//Player not moving
+		else {
+			animation.CrossFade(idle.name);
+		}
+	}
+}
---
+++ Second Soul/Assets/Mob.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour {
+
+	public float speed;
+	public float range;
+	
+	public CharacterController controller;
+	public Transform player;
+	private PlayerCombat hero;
+
+	public AnimationClip attackClip;
+	public AnimationClip run;
+	public AnimationClip idle;
+	public AnimationClip die;
+
+	public double health;
+	public double damage;
+
+	public float impactTime;
+
+	public bool impacted;
+	
+	// Use this for initialization
+	void Start () {
+		//health = 100;
+		hero = player.GetComponent<PlayerCombat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (inRange());
+		//Debug.Log (health);
+		if (!isDead ()) {
+			if (!inRange ()) {
+				chase ();
+			} 
+			else {
+				//animation.CrossFade (idle.name);
+				attack();
+			}
+		} 
+		else {
+			dieMethod();
+		}
+	}
+	
+	public bool inRange(){
+		return Vector3.Distance(transform.position, player.position)<range;
+	}
+
+	private void attack(){
+		if (!hero.isDead ()) {
+			animation.CrossFade (attackClip.name);
+
+			if (animation [attackClip.name].time > animation [attackClip.name].length * impactTime && !impacted && animation [attackClip.name].time < 0.90 * animation [attackClip.name].length) {
+				hero.getHit (damage);
+				impacted = true;
+			}
+
+			if (animation [attackClip.name].time > 0.90 * animation [attackClip.name].length) {
+				impacted = false;
+			}
+		}
+	}
+
+	private void chase(){
+		transform.LookAt (player.position);
+		controller.SimpleMove (transform.forward * speed);
+		animation.CrossFade (run.name);
+	}
+
+	public void getHit(double damage){
+		health -= damage;
+
+		if (health <= 0) {
+			health = 0;
+		}
+	}
+
+	public bool isDead(){
+		if (health <= 0) {
+			return true;
+		}
+		return false;
+	}
+
+	private void dieMethod(){
+		Destroy(controller);
+
+		player.GetComponent<PlayerCombat> ().enemy = null;
+		
+		animation.CrossFade (die.name);
+		
+
+		if (animation[die.name].time > animation[die.name].length * 0.80) {
+			animation[die.name].speed = 0;
+		}
+	}
+
+	void OnMouseOver(){
+		//Debug.Log ("Mouse is over");
+		if (!isDead ()) {
+			player.GetComponent<PlayerCombat> ().enemy = gameObject;
+		}
+	}
+
+	void OnMouseExit(){
+		player.GetComponent<PlayerCombat> ().enemy = null;
+	}
+}
