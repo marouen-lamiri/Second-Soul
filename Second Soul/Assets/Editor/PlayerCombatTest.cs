11aaffefc9fdad3f6ebeca22124d92207f652c6e
smileyWolf <dan.magariu@hotmail.com>
2014-10-10 19:07:45 -0400

    Updated; working version: Unit test for fighting/damaging an enemy.
    Reference: US 1.4: Be able to fight monsters

---
+++ Second Soul/Assets/Editor/PlayerCombatTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+public class PlayerCombatTest {
+
+	public PlayerCombat player = new PlayerCombat ();
+	public Mob enemy = new Mob ();
+
+	[Test]
+	public void DamageEnemy ([Random(1, 100, 5)] int initHealth, [Random(1, 20, 5)] int damageValue) {
+		// Arrange
+		// 1. Enemy
+		enemy.health = initHealth;
+
+		// 2. Player
+		player.damage = damageValue;
+
+		// Act
+		enemy.getHit(player.damage);
+
+		// Assert
+		Assert.That (enemy.health < initHealth);
+	}
+
+}
