66d2988d1d5abc8eb7087f3d6c1a3ce85a524fb3
smileyWolf <dan.magariu@hotmail.com>
2014-10-10 11:42:22 -0400

    US 1.4.1: Be able to kill monsters

---
+++ Second Soul/Assets/Editor/PlayerCombatTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+public class PlayerCombatTest {
+
+	public PlayerCombat player;
+	public Mob enemy;
+
+	#if false
+		[Test]
+		public void EnemyInRange ()
+		{
+				// Arrange
+				// Act
+				// Assert
+		}
+	#endif
+
+	[Test]
+	public void DamageEnemy () {
+		// Arrange
+		// 1. Create player & enemy.
+		player = new PlayerCombat ();
+		enemy = new Mob ();
+
+		// 2. Enemy health.
+		var initHealth = enemy.health;
+
+		// Act
+		enemy.GetComponent<Mob>().getHit(player.damage);
+
+		// Assert
+		Assert (enemy.health < initHealth);
+	}
+
+}
