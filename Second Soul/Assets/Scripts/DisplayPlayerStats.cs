13712c89be83de5a8e7d8c98dea902a4ab5f3ddd
Marouen Lamiri <marouen.lamiri@gmail.com>
2014-10-10 01:50:16 -0400

    Added Character Stats
    Press s to see the character's stats but an issue is still present since
    the box is drawn several time. It's an issue likely related to frames

---
+++ Second Soul/Assets/Scripts/DisplayPlayerStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPlayerStats : MonoBehaviour {
+
+	public PlayerCombat player;
+	public bool isStatsDisplayed = false;
+	public float countdown = 3.0f;
+	
+	void Update()
+	{
+		if (Input.GetKey ("s")) 
+		{
+			boolChange ();
+		}
+	}
+
+	void OnGUI () {
+		if (isStatsDisplayed) {
+			GUI.Box (new Rect (10, 10, 150, 150), "Character Stats");
+			GUI.Label (new Rect (20, 40, 80, 20), "Health: " + player.health);
+			GUI.Label (new Rect (20, 70, 80, 20), "Energy: " + player.energy); 
+		}
+	}
+
+	void boolChange ()
+	{
+		if (isStatsDisplayed) {
+			isStatsDisplayed = false;
+		} 
+		else {
+			isStatsDisplayed = true;
+		}
+	}
+}
--- Second Soul/Assets/Scripts/Pausing.js
+++ Second Soul/Assets/Scripts/Pausing.js
@@ -22,3 +22,11 @@ function Pause()
     	isPaused = true;
     }
 }
+
+function OnGUI()
+{
+	 if(isPaused)
+	 {
+	 	GUI.Label (new Rect (Screen.width/2 - 75, Screen.height/2 - 25, 200, 100),"<size=40>-Paused-</size>");
+	 }
+}
