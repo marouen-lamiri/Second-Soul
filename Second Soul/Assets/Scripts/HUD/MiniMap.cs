aef6048af2f00de2ccb05a811ea0926ecf8f2762
smileyWolf <danutzmd@gmail.com>
2014-12-13 03:03:53 -0500

    Fixed the Mini-Map, so it is under the HUB and not under the Fighter (Mini-Map #80.) Added an appropriate script for that.

---
+++ Second Soul/Assets/Scripts/HUD/MiniMap.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+	public Transform Fighter; // The Fighter from the scene.
+
+	void Update()
+	{
+		// Adjusting the position of the MiniMap to the Fighter's one.
+		this.transform.position = new Vector3 (Fighter.position.x, transform.position.y, Fighter.position.z);
+	}
+}
