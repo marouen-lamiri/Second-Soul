669d4867f4e71eb23abb1dbc3a879ca701483065
Marouen Lamiri <marouen.lamiri@gmail.com>
2014-12-29 14:39:49 -0500

    Update #63 and #66: - Fixed speed bug for enemy - Fixed stopping enemy for pathfinding - Improved structured of prefab creating in Hierarchy

--- Second Soul/Assets/Scripts/Character/Character.cs
+++ Second Soul/Assets/Scripts/Character/Character.cs
@@ -255,7 +255,7 @@ public abstract class Character : MonoBehaviour {
 		List<Vector3> path = grid.worldFromNode(grid.path);
 		followPath (path);
 		Vector3 destination;
-		if (Vector3.Distance (transform.position, target.transform.position) > 2) {
+		if (Vector3.Distance (transform.position, target.transform.position) > 4) {
 			destination = path[1];
 		} 
 		else {
@@ -266,7 +266,7 @@ public abstract class Character : MonoBehaviour {
 		newRotation.z = 0;
 		
 		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 7);
-		controller.SimpleMove (transform.forward * speed);
+		controller.SimpleMove (transform.forward * speed *4);
 	}
 	
 	public void attack(){
