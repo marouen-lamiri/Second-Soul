6c4da5d7adb11d379a9ab1bb01d38e4589a05320
Guillaume Fortin <xguillaumex@hotmail.com>
2015-01-21 17:16:41 -0500

    Initial Setup for issues #85 includes some art snippets and basic structure

--- Second Soul/Assets/Scripts/Skill/SkillTree.cs
+++ Second Soul/Assets/Scripts/Skill/SkillTree.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class SkillTree : MonoBehaviour {
 
+	bool isSkillOpen;
+	
+	public Rect position;
+	public Texture2D image;
+	
 	// Use this for initialization
 	void Start () {
-	
+		isSkillOpen = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown ("t")) {
+			toggleTab();
+		}
+	}
 	
+	void OnGUI(){
+		if(isSkillOpen){
+			drawSkillTree();
+		}
+	}
+	
+	private void toggleTab(){
+		isSkillOpen = !isSkillOpen;
+	}
+	
+	private void drawSkillTree(){
+		position.x = Screen.width - position.width;
+		position.y = Screen.height - position.height - Screen.height * 0.2f;
+		GUI.DrawTexture(position, image);
 	}
 }
