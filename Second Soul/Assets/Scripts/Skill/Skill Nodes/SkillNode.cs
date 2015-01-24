1f57f2ab290565361fb5c5933f45138baf69fbdd
gfortin13 <xguillaumex@hotmail.com>
2015-01-24 17:00:36 -0500

    Added art and model classes/objects for issue #85

--- Second Soul/Assets/Scripts/Skill/Skill Nodes/SkillNode.cs
+++ Second Soul/Assets/Scripts/Skill/Skill Nodes/SkillNode.cs
@@ -9,13 +9,14 @@ public class SkillNode {
 	string skillName;
 	string skillDesc;
 	
-	Rect position;
-	Texture2D icon;
+	public Rect position;
+	public Texture2D icon;
 	
-	public SkillNode(ISkill s, string name, string desc){
+	public SkillNode(ISkill s, string name, string desc, Rect pos, Texture2D icon){
 		this.skill = s;
 		this.skillName = name;
 		this.skillDesc = desc;
+		this.position = pos;
+		this.icon = icon;
 	}
-
 }
--- Second Soul/Assets/Scripts/Skill/Skill Nodes/SkillTreeNode.cs
+++ Second Soul/Assets/Scripts/Skill/Skill Nodes/SkillTreeNode.cs
@@ -8,9 +8,27 @@ public class SkillTreeNode : SkillNode {
 	bool unlocked;
 	bool available;
 	
-	public SkillTreeNode(ISkill s, string name, string desc) : base(s, name, desc){
+	List<SkillTreeNode> parents;
+	List<SkillTreeNode> children;
+	
+	public SkillTreeNode(ISkill s, string name, string desc, Rect pos, Texture2D icon) 
+	: base(s, name, desc, pos, icon){
 		this.unlocked = false;
 		this.available = false;
+	}
+	
+	public void makeChildrenAvailable(){	
+		foreach(SkillTreeNode child in children){
+			bool allParentsUnlocked = true; // turns false if any are not
+			foreach(SkillTreeNode parent in parents){
+				if(!parent.unlocked){
+					allParentsUnlocked = false;
+				}
+			}
+			if(allParentsUnlocked){
+				child.available = true;
+			}
+		}
 	}
 
 }
