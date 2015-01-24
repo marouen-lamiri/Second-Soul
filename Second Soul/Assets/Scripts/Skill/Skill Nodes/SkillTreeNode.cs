738de2c581105593114cd9efd4206ac0abb425a6
gfortin13 <xguillaumex@hotmail.com>
2015-01-24 15:43:39 -0500

    creating basic classes for issue #85

---
+++ Second Soul/Assets/Scripts/Skill/Skill Nodes/SkillTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SkillTreeNode : SkillNode {
+
+	bool unlocked;
+	bool available;
+	
+	public SkillTreeNode(ISkill s, string name, string desc) : base(s, name, desc){
+		this.unlocked = false;
+		this.available = false;
+	}
+
+}
