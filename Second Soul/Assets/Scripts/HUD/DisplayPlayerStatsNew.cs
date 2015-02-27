22841f9ee8c99b4e44128d2b77626ee1ada70a38
smileyWolf <danutzmd@gmail.com>
2015-02-27 03:00:38 -0500

    Improvements for Player's Stats display referred to US #88 (still under development); also related to #74.

--- Second Soul/Assets/Scripts/HUD/DisplayPlayerStatsNew.cs
+++ Second Soul/Assets/Scripts/HUD/DisplayPlayerStatsNew.cs
@@ -6,9 +6,9 @@ public class DisplayPlayerStatsNew : MonoBehaviour {
 	// Constants
 	public const int BOX_WIDTH         = 200;
 	public const int LABEL_WIDTH       = 150;
-	public const int BUTTON_WIDTH      = 150;
+	public const int BUTTON_WIDTH      = 100;
 	public const int LABEL_HEIGHT      = 20;
-	public const int BUTTON_HEIGHT     = 50;
+	public const int BUTTON_HEIGHT     = 30;
 	public const int LABLE_HEIGHT_MIN  = 20;
 	public const int BUTTON_HEIGHT_MIN = 20;
 	public const int SPACE_BETWEEN     = 10;
@@ -27,19 +27,73 @@ public class DisplayPlayerStatsNew : MonoBehaviour {
 	enum secondaryStatsLabels {Armor, FireResistance, Accuracy, AttackSpeed};
 	enum primaryStatsButtons {Details};
 	
+	Hashtable primaryStatsTooltip;
+	Hashtable secondaryStatsTooltip;
+
 	// Lengths of the all enums.
 	int primaryLength   = System.Enum.GetValues (typeof(primaryStatsLabels)).Length;
 	int secondaryLength = System.Enum.GetValues (typeof(secondaryStatsLabels)).Length;
-	int buttonsLength   = System.Enum.GetValues (typeof(primaryStatsButtons)).Length;
+	int primaryButtonsLength   = System.Enum.GetValues (typeof(primaryStatsButtons)).Length;
 	
 	// Arrays with the names of all enums.
 	string[] namesPrimary   = System.Enum.GetNames(typeof(primaryStatsLabels));
 	string[] namesSecondary = System.Enum.GetNames(typeof(secondaryStatsLabels));
-	string[] namesButtons   = System.Enum.GetNames(typeof(primaryStatsButtons));
+	string[] namesPrimaryButtons   = System.Enum.GetNames(typeof(primaryStatsButtons));
 
 	void Start ()
 	{
+		primaryStatsTooltip = new Hashtable();
+
+		primaryStatsTooltip.Add (primaryStatsLabels.Level, "Level is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Class, "Class is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Strength, "Strength is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Dexterity, "Dexterity is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Endurance, "Endurance is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Health, "Health is ...");
+		primaryStatsTooltip.Add (primaryStatsLabels.Energy, "Energy is ...");
 	}
+
+//	private string getTooltip(primaryStatsLabels enumValue)
+//	{
+//		string enumTooltip = "";
+//		
+//		switch (enumValue)
+//		{
+//			case primaryStatsLabels.Level:
+//				enumTooltip =  "Level is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Class:
+//				enumTooltip =  "Class is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Strength:
+//				enumTooltip =  "Strength is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Dexterity:
+//				enumTooltip =  "Dexterity is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Endurance:
+//				enumTooltip =  "Endurance is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Health:
+//				enumTooltip =  "Health is ...";
+//				break;
+//				
+//			case primaryStatsLabels.Energy:
+//				enumTooltip =  "Energy is ...";
+//				break;
+//				
+//			default:
+//				enumTooltip =  "";
+//				break;
+//		}
+//		
+//		return enumTooltip;
+//	}
 	
 	// Update is called once per frame, checks if 's'-key is pressed.
 	void Update ()
@@ -67,7 +121,6 @@ public class DisplayPlayerStatsNew : MonoBehaviour {
 	// Returning the height of the box that contains the labels and buttons.
 	private int getBoxHeight (int numberLabels)
 	{
-		
 		int preferableBoxHeight = (numberLabels * LABEL_HEIGHT) + ((numberLabels + 1) * SPACE_BETWEEN) + (BUTTON_HEIGHT + SPACE_BETWEEN);
 
 		return preferableBoxHeight;
@@ -75,7 +128,6 @@ public class DisplayPlayerStatsNew : MonoBehaviour {
 	
 	void OnGUI()
 	{
-		
 		// Screen percentage: 50% of the screen.
 		int screenWidth50  = Screen.width / 2;
 		int screenHeight50 = Screen.height / 2;
@@ -93,43 +145,31 @@ public class DisplayPlayerStatsNew : MonoBehaviour {
 			// Drawing the Lables.
 			for (i = 0; i < primaryLength; i++)
 			{
-				//GUI.Label(new Rect (screenWidth50 - LABEL_WIDTH / 2, boxHeightPosition + (i * LABEL_HEIGHT) + ((i + 1) * SPACE_BETWEEN), LABEL_WIDTH, LABEL_HEIGHT), namesPrimary[i], labelStyle);
-				GUI.Label(new Rect (screenWidth50 - LABEL_WIDTH / 2, boxHeightPosition + (i * LABEL_HEIGHT) + ((i + 1) * SPACE_BETWEEN), LABEL_WIDTH, LABEL_HEIGHT), namesPrimary[i]);
-
-//				if (GUI.Button(new Rect (screenWidth50 - BUTTON_WIDTH / 2, boxHeightPosition + (i * BUTTON_HEIGHT) + ((i + 1) * SPACE_BETWEEN), BUTTON_WIDTH, BUTTON_HEIGHT), namesButtons[i]))
-//				{
-//					/* Need to add an implementation for each (new) button in "primaryStatsButtons"-enum. */
-//					
-//					// Resume
-//					if (string.Equals(namesButtons[i], System.Enum.GetName(typeof(primaryStatsButtons), primaryStatsButtons.Resume)))
-//					{
-//						isStatsPressed = false;
-//					}
-//					
-//					// Options
-//					if (string.Equals(namesButtons[i], System.Enum.GetName(typeof(primaryStatsButtons), primaryStatsButtons.Options)))
-//					{
-//						Debug.Log("Options-button pressed; no implementation yet.");
-//					}
-//					
-//					// Quit
-//					if (string.Equals(namesButtons[i], System.Enum.GetName(typeof(primaryStatsButtons), primaryStatsButtons.Quit)))
-//					{
-//						Application.Quit();
-//					}
-//				}
+				GUI.Label(new Rect (screenWidth50 - LABEL_WIDTH / 2, boxHeightPosition + (i * LABEL_HEIGHT) + ((i + 1) * SPACE_BETWEEN), LABEL_WIDTH, LABEL_HEIGHT), new GUIContent(namesPrimary[i], (string)primaryStatsTooltip[(primaryStatsLabels)System.Enum.Parse(typeof(primaryStatsLabels), namesPrimary[i])])/*, labelStyle*/);
 			}
+
 			int j;
 			// Drawing the Buttons.
-			for (j = 0; j < buttonsLength; j++)
+			for (j = 0; j < primaryButtonsLength; j++)
 			{
-				GUI.Button(new Rect (screenWidth50 - BUTTON_WIDTH / 2, boxHeightPosition + (i * LABEL_HEIGHT) + ((i + 1) * SPACE_BETWEEN), BUTTON_WIDTH, BUTTON_HEIGHT), namesButtons[j]);
+				if (GUI.Button(new Rect (screenWidth50 - BUTTON_WIDTH / 2, boxHeightPosition + (i * LABEL_HEIGHT) + ((i + 1) * SPACE_BETWEEN), BUTTON_WIDTH, BUTTON_HEIGHT), namesPrimaryButtons[j]))
+				{
+					// Details
+					if (string.Equals(namesPrimaryButtons[j], System.Enum.GetName(typeof(primaryStatsButtons), primaryStatsButtons.Details)))
+					{
+						// Will open the Details-box.
+					}
+				}
 			}
 
-			GUI.Box(new Rect(5, 35, 110, 75), new GUIContent("Box", "this box has a tooltip"));
-			GUI.Button(new Rect(10, 80, 100, 20), "No tooltip here");
-			GUI.Button(new Rect(10, 150, 100, 20), new GUIContent("I have a tooltip", "The button overrides the box"));
-			GUI.Label(new Rect(10, 190, 100, 40), GUI.tooltip);
+			float x = Event.current.mousePosition.x;
+			float y = Event.current.mousePosition.y; 
+
+			GUI.Label(new Rect(x, y - 2*SPACE_BETWEEN, 100, 40), GUI.tooltip);
+//			GUI.Box(new Rect(5, 35, 110, 75), new GUIContent("Box", "this box has a tooltip"));
+//			GUI.Button(new Rect(10, 80, 100, 20), "No tooltip here");
+//			GUI.Button(new Rect(10, 150, 100, 20), new GUIContent("I have a tooltip", "The button overrides the box"));
+//			GUI.Label(new Rect(10, 190, 100, 40), GUI.tooltip);
 		}
 	}
 }
