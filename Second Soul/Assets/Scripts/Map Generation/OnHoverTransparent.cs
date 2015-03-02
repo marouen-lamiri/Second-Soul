2859057034a7e86ba51fa5bdac783baa5bfcd966
Marouen Lamiri <marouen.lamiri@gmail.com>
2015-03-01 19:53:15 -0500

    Update: Remove magic numbers in map generation, also added on mouse hover transparency of the wall

---
+++ Second Soul/Assets/Scripts/Map Generation/OnHoverTransparent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHoverTransparent : MonoBehaviour {
+
+	GameObject mainCamera;
+	Player player;
+	Color initialColor;
+
+	// Use this for initialization
+	void Start () {
+		initialColor = transform.renderer.material.color;
+		player = GameObject.FindObjectOfType(typeof(Fighter))as Fighter;
+		mainCamera = GameObject.Find("MainCamera") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+	}
+
+	void OnMouseEnter(){
+		transform.renderer.material.color = new Color(initialColor.r,initialColor.g,initialColor.b,0.5f);
+	}
+	
+	void OnMouseExit(){
+		transform.renderer.material.color = initialColor;
+	}
+}
