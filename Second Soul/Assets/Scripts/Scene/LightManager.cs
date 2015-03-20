a8333161bf178fedb2e57501dd48629ea2fa5249
Marouen Lamiri <marouen.lamiri@gmail.com>
2015-03-20 12:31:44 -0400

    Fixed lighting issue in town and dungeon

---
+++ Second Soul/Assets/Scripts/Scene/LightManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightManager : MonoBehaviour {
+
+	string sceneName = "SkillTree";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Application.loadedLevelName == sceneName){
+			this.light.range = 30;
+		}
+		else{
+			this.light.range = 0;
+		}
+	}
+}
