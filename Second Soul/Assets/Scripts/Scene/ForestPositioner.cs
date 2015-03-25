abc3a5c39142986bea51d9d3fc69b5ed9893d6f7
Marouen Lamiri <marouen.lamiri@gmail.com>
2015-03-25 18:52:17 -0400

    Update: Fixed bug related to boss scene #98, same for town and forest #90

---
+++ Second Soul/Assets/Scripts/Scene/ForestPositioner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestPositioner : MonoBehaviour {
+
+	private Fighter fighter;
+	private Sorcerer sorcerer;
+	private Vector3 initialFighterPosition = new Vector3(100,0,27);
+	private Vector3 initialSorcererPosition = new Vector3(99,0,22);
+	
+	// Use this for initialization
+	void Start () {
+		fighter = (Fighter)GameObject.FindObjectOfType (typeof (Fighter));
+		sorcerer = (Sorcerer)SorcererInstanceManager.getSorcerer ();
+		fighter.transform.position = initialFighterPosition;
+		sorcerer.transform.position = initialSorcererPosition;
+	}
+}
---
+++ Second Soul/Assets/Scripts/Scene/playerPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerPosition : MonoBehaviour {
+
+	private Fighter fighter;
+	private Sorcerer sorcerer;
+	private Vector3 initialFighterPosition = new Vector3(25,0,23);
+	private Vector3 initialSorcererPosition = new Vector3(24,0,22);
+
+	// Use this for initialization
+	void Start () {
+		fighter = (Fighter)GameObject.FindObjectOfType (typeof (Fighter));
+		sorcerer = (Sorcerer)SorcererInstanceManager.getSorcerer ();
+		fighter.transform.position = initialFighterPosition;
+		sorcerer.transform.position = initialSorcererPosition;
+	}
+}
